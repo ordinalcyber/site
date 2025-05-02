7aeb629e2f15b05359b5d778e5bee4aa9dbfc299
ordinalcyber <tominoucorbac24@gmail.com>
2025-05-02 16:52:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("index.html", title="Mon Site SEO", description="Un site Flask déployé sur Render et référencé sur Google.")
 
 if __name__ == "__main__":
     app.run(debug=True)
