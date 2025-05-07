7403feb48edb6325e447951d6d869475539cfe7d
ordinalcyber <tominoucorbac24@gmail.com>
2025-05-07 14:47:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ def ouvertures():
 @app.route("/coaching")
 def coaching():
     return render_template("coaching.html")
-@app.route('/echquier')
-def echquier():
+@app.route('/echiquier')
+def echiquier():
     # code pour afficher l'échiquier
-    return render_template('echquier.html')
+    return render_template('echiquier.html')
 async def keep_alive():
     requests.get(URL)
     await asyncio.sleep(60)
